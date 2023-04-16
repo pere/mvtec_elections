8f90dd35070c1669ec9329375494c26972a927e7
Matt Osborn <mattjosborn@gmail.com>
2023-04-16 13:29:57 +0200

    color scale fixes

--- src/App.svelte
+++ src/App.svelte
@@ -1,93 +1,109 @@
 <script>
-	import Navbar from './components/Navbar.svelte';
-	import Map from './components/Map.svelte';
-  import LateralMenu from './components/LateralMenu.svelte';
-  import * as d3 from 'd3';
-  import BasicStats from './components/BasicStats.svelte';
-   import ScaleForm from './components/ScaleForm.svelte'; 
-   import { create_scale2 } from "./components/BasicStats.svelte";
-  import {observable_data}  from './data/observable_data.js';
-  
-  import { onMount } from 'svelte';
+  import Navbar from "./components/Navbar.svelte";
+  import Map from "./components/Map.svelte";
+  import LateralMenu from "./components/LateralMenu.svelte";
+  import * as d3 from "d3";
+  import BasicStats from "./components/BasicStats.svelte";
+  import ScaleForm from "./components/ScaleForm.svelte";
+  import { create_scale2 } from "./components/BasicStats.svelte";
+  import { observable_data } from "./data/observable_data.js";
 
+  import { onMount } from "svelte";
 
-  let years=[];
-  let main_parties=[];
-  $: selected_schema='interpolateWarm';
-  let selectedYear = ''; //// menu built from data
-  let selectedParty = ''; //// menu built from data
+  let years = [];
+  let main_parties = [];
+  $: selected_schema = "interpolateWarm";
+  let selectedYear = ""; //// menu built from data
+  let selectedParty = ""; //// menu built from data
 
-  const getYears= () =>
-  {
- /*    console.info(
+  const createColorScale = (selected_schema, min, max) => {
+    return d3.scaleSequential(d3[selected_schema]).domain([min, max]);
+  };
+
+  const getYears = () => {
+    /*    console.info(
       observable_data.map(d=>d.year)
     ) */
-    years=[...new Set(observable_data.map(d=>d.year))];
-    years=years.sort((a,b)=>a-b);
+    years = [...new Set(observable_data.map((d) => d.year))];
+    years = years.sort((a, b) => a - b);
     //return years;
-  }
+  };
 
-  const getMainParties= () =>
-  {
- /*    console.info(
+  const getMainParties = () => {
+    /*    console.info(
       observable_data.map(d=>d.year)
     ) */
-    main_parties=[...new Set(observable_data.map(d=>d.main_party))];
-    main_parties=main_parties.sort((a,b)=>a-b);
-    
-  }
+    main_parties = [...new Set(observable_data.map((d) => d.main_party))];
+    main_parties = main_parties.sort((a, b) => a - b);
+  };
 
-  $:filtered_data=observable_data.filter(d=>d.year==selectedYear && d.main_party==selectedParty);
-  
-  $: console.log(filtered_data)
-
-  
-  
-  onMount(() => 
-  {
-  getYears();
-  getMainParties();
-
-  console.log(main_parties)
-  }
+  $: filtered_data = observable_data.filter(
+    (d) => d.year == selectedYear && d.main_party == selectedParty
   );
-</script>
-<svelte:head>
-  
-  <!-- <script src="./js/topojson.v1.min.js"></script> -->
 
+  $: f = filtered_data
+    .filter((d) => d.voted_proportion > 0)
+    .map((d) => d.voted_proportion);
+
+  $: stats = {
+    min: d3.min(f),
+    max: d3.max(f),
+    avg: d3.mean(f),
+  };
+
+  $: colorScale = createColorScale(selected_schema, stats.min, stats.max);
+
+  $: console.log(filtered_data);
+
+  onMount(() => {
+    getYears();
+    getMainParties();
+
+    console.log(main_parties);
+  });
+</script>
+
+<svelte:head>
+  <!-- <script src="./js/topojson.v1.min.js"></script> -->
 </svelte:head>
 <div class="app">
   <!-- <Navbar/> -->
   <section class="lateralMenu">
-  <LateralMenu bind:selectedYear {years}/>
-  {#if selectedYear}
-  <LateralMenu bind:selectedParty {main_parties}/>  
-
-  {/if}
-
-  {#if selectedParty && selectedYear && selected_schema}
- 
-  <BasicStats {filtered_data} sel_schema=selected_schema  {selected_schema}/>
-  {#if filtered_data.length>0}
-    <ScaleForm bind:selected_schema sel_schema=selected_schema on:change={create_scale2(selected_schema,filtered_data)}/> 
+    <LateralMenu bind:selectedYear {years} />
+    {#if selectedYear}
+      <LateralMenu bind:selectedParty {main_parties} />
     {/if}
-  {/if}
-</section>
-	<Map {filtered_data} {selected_schema}/>
+
+    {#if selectedParty && selectedYear && selected_schema}
+      <BasicStats
+        {filtered_data}
+        sel_schema="selected_schema"
+        {colorScale}
+        {stats}
+      />
+      {#if filtered_data.length > 0}
+        <ScaleForm
+          bind:selected_schema
+          sel_schema="selected_schema"
+          on:change={create_scale2(selected_schema, filtered_data)}
+        />
+      {/if}
+    {/if}
+  </section>
+  <Map {filtered_data} {colorScale} />
 </div>
+
 <style>
-    path.domain,line 
-    {
-        opacity: 0;
-    }
-.lateralMenu
-{
-    width:30vw;
-    
+  path.domain,
+  line {
+    opacity: 0;
+  }
+  .lateralMenu {
+    width: 30vw;
+
     height: calc(100vh - 77px);
     background-color: black;
-}
+  }
 
   .app {
     display: flex;
@@ -101,7 +117,6 @@
     display: none;
   }
   .app > div {
-    
     vertical-align: top;
   }
 </style>
